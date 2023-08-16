68faf828091e996ec7e5c14ff839b03420316270
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-08-17 00:36:37 +0530

    Create SolidityMemoryLayout.sol

---
+++ Memory_Layout/SolidityMemoryLayout.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SolidityMemoryLayout {
+    /**
+    * Layout in Memory(Reserves certain areas of memory) :
+        - First 64 bytes (0x00 to 0x3f) used for storing temporarily data while performing hash calculations
+        - Next 32 bytes (0x40 to 0x5f) also known as "free memory pointer" keeps track of next available location in memory where new data can be stored
+        - Next 32 bytes (0x60 to 0x7f) is a zero slot that is used as starting point for dynamic memory arrays that is initialized with 0 and should never be written to.
+    */
+    function freeMemory() external {
+        assembly {
+            // fetches the next free available slots in the memory
+            let freeMemPtr := mload(0x40)
+
+            // Free memory pointer is auto. managed by the solidity but need to manually update while using Yul
+            mstore(freeMemPtr, 1)
+            mstore(add(freeMemPtr, 0x20), 2) // adds 32 bytes to the freeMemPtr offset and then stores the new value
+            mstore(add(freeMemPtr, 0x40), 3) // similarly here we leaps 2 slots(one for the previous and one for this) and then stores the value
+            mstore8(add(freeMemPtr, 0x48), 4) // can read memory in 32 bytes while writing can be in 8 or 32 bytes
+
+            // mstore(offset ,4) : 0x0000000000000000000000000000000000000000000000000000000000000004
+            // mstore8(offset,4) : 0x0400000000000000000000000000000000000000000000000000000000000000
+
+            // update the free memory pointer by the length already occupied 3 slots + 8 bytes = 0x68
+            updateFreeMemPtr(0x68)
+
+            // updates free memory pointer that gets the occupied slots length of the memory added as input
+            function updateFreeMemPtr(length) {
+                let previousFreeMemPtr := mload(0x40)
+                mstore(0x40, add(previousFreeMemPtr, length))
+            }
+
+            // msize : returns the largest accessed memory index
+            // msize does not guarantee unused memory until the free pointer is updated
+        }
+    }
+
+    /**
+     * Memory Expansion costs : EVM doesn't allocate the entire memory upfront.
+     *  It expands as needed, and cost gas only for the expansion.
+     * memory_size_word = (memory_byte_size + 31) / 32
+     * memory_cost = (memory_size_word ** 2) / 512 + (3 * memory_size_word)
+     * memory_expansion_cost = new_memory_cost - last_memory_cost
+     */
+    function exceedMemoryGasLimit() public pure returns (uint256 storedVal) {
+        assembly {
+            storedVal := mload(mul(125000, 0x20)) // gives Out of gas error once we hit 30MN Ethereum Block gas limit
+        }
+        // for memory_size_word ~= 123200 bytes, 30MN gas limit will reach
+    }
+}
